9df88e1d8293299c863a47d896ded95ed3edefbd
Jay patel <jaypatel.learn@gmail.com>
2023-08-05 12:03:45 +0530

    prepare base model complated

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -1,8 +1,20 @@
 from pydantic import BaseModel
 from pathlib import Path
 
+
 class DataIngestionConfig(BaseModel):
     root_dir: Path
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+
+class PrepareBaseModelConfig(BaseModel):
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
