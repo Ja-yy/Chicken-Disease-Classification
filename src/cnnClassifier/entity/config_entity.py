26c192010ef27a3723d3780dd0a6c5bbce88f415
Jay patel <jaypatel.learn@gmail.com>
2023-08-05 15:11:48 +0530

    training added

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -24,3 +24,14 @@ class PrepareCallbacksConfig(BaseModel):
     root_dir: Path
     tensorboard_root_log_dir: Path
     checkpoint_model_filepath: str
+
+
+class TrainingConfig(BaseModel):
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
