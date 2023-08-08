b1a4977fa2932d51d4d2cf3d85d2d3dad2a79c1a
Jay patel <jaypatel.learn@gmail.com>
2023-08-08 11:48:07 +0530

    fastapi added

--- main.py
+++ main.py
@@ -1,52 +1,39 @@
-from cnnClassifier import logger
-from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from cnnClassifier.pipeline.stage_02_prepare_base_model import (
-    PrepareBaseModelTrainingPipeline,
+import os
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+
+from cnnApp.train import router as train_router
+from cnnApp.predict import router as predict_router
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Add CORS middleware to allow cross-origin requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
-from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
-
-STAGE_NAME = "Data Ingestion stage"
-try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    data_ingestion = DataIngestionTrainingPipeline()
-    data_ingestion.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-    logger.exception(e)
-    raise e
-
-STAGE_NAME = "Prepare base model"
-try:
-    logger.info(f"*******************")
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    prepare_base_model = PrepareBaseModelTrainingPipeline()
-    prepare_base_model.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-    logger.exception(e)
-    raise e
 
 
-STAGE_NAME = "Training"
-try:
-    logger.info(f"*******************")
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    model_trainer = ModelTrainingPipeline()
-    model_trainer.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-    logger.exception(e)
-    raise e
+@app.get("/")
+async def render_template(request: Request):
+    template_name = "index.html"
+    context = {"request": request}
+    return templates.TemplateResponse(template_name, context)
 
-STAGE_NAME = "Evaluation stage"
-try:
-    logger.info(f"*******************")
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    model_evalution = EvaluationPipeline()
-    model_evalution.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
-except Exception as e:
-    logger.exception(e)
-    raise e
+app.include_router(train_router)
+app.include_router(predict_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
